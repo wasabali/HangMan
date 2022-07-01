4ee43364c044285e8873beeb26e6a984e39b01ab
wasabali <60469868+wasabali@users.noreply.github.com>
2022-07-01 12:46:04 +0200

    Update Player.cs

--- Hangman/Player.cs
+++ Hangman/Player.cs
@@ -1,10 +1,28 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+
 namespace Hangman
 {
-    public class Player
+    class Player
     {
-        public Player()
+        public string Name { get; private set; }
+
+        private int score;
+        public int Score
         {
+            get { return score; }
+            set
+            {
+                if (value > 0)
+                    score = value;
+            }
+        }
+
+        public List<char> GuessedLetters { get; } = new List<char>();
+
+        public Player(string name)
+        {
+            this.Name = name;
         }
     }
 }
